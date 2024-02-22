b6c4d220c4ece49c053cffdc46b784f68cc5345c
PayamAnari <anari.p62@gmail.com>
2024-02-22 14:29:19 +0100

    Add server

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
-
+import allTypeDefs from './schemas/index.schema.js';
+import allResolvers from './resolvers/index.resolver.js';
+import context from './context/context.js';
 
 dotenv.config();
 
 const server = new ApolloServer({
-   typeDefs: 
-})
+  typeDefs: allTypeDefs,
+  resolvers: allResolvers,
+  includeStacktraceInErrorResponses: false,
+  introspection: true,
+});
