2a797dd876497c7edcf138329e02ccfc6e8582ae
PayamAnari <anari.p62@gmail.com>
2024-02-25 14:47:57 +0100

    Add helper folder

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    following: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const UserModel = model('User', userSchema);
+export default UserModel;
