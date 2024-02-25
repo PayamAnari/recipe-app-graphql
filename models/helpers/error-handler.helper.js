122c7ecffac887ce8b45c71203663c561e0e6efa
PayamAnari <anari.p62@gmail.com>
2024-02-25 15:00:30 +0100

    Modify errorhandler

--- models/helpers/error-handler.helper.js
+++ models/helpers/error-handler.helper.js
@@ -1,15 +1,40 @@
 import { ApolloServerErrorCode } from '@apollo/server/errors';
-import { GraphqlError } from 'graphql';
+import { GraphQLError } from 'graphql';
 
 export const ErrorTypes = {
   BAD_USER_INPUT: {
-    errorMessage: 'The input provided is invalid.',
     errorCode: ApolloServerErrorCode.BAD_USER_INPUT,
     errorStatus: 400,
   },
   BAD_REQUEST: {
-    errorMessage: 'The request is invalid.',
     errorCode: ApolloServerErrorCode.BAD_REQUEST,
     errorStatus: 400,
   },
+  NOT_FOUND: {
+    errorCode: 'NOT_FOUND',
+    errorStatus: 404,
+  },
+  UNAUTHORIZED: {
+    errorCode: 'UNAUTHORIZED',
+    errorStatus: 401,
+  },
+  ALREADY_EXISTS: {
+    errorCode: 'ALREADY_EXISTS',
+    errorStatus: 409,
+  },
+  INTERNAL_SERVER_ERROR: {
+    errorCode: ApolloServerErrorCode.INTERNAL_SERVER_ERROR,
+    errorStatus: 500,
+  },
+};
+
+export default (errorMessage, errorType) => {
+  throw new GraphQLError(errorMessage, {
+    extensions: {
+      code: errorType.errorcode,
+      http: {
+        status: errorType.errorStatus,
+      },
+    },
+  });
 };
