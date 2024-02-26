89e97d4bb995b8eee2bddade2d63ab5e055065b1
PayamAnari <anari.p62@gmail.com>
2024-02-26 13:19:56 +0100

    Create recipeResolver func

--- resolvers/recipe.resolvers.js
+++ resolvers/recipe.resolvers.js
@@ -3,3 +3,21 @@ import RecipeHelper from '../helpers/recipe.helper.js';
 import throwCustomError, {
   ErrorTypes,
 } from '../helpers/error-handler.helper.js';
+
+const recipeResolver = {
+  Query: {
+    recipe: async (parent, { id }, contextValue) => {
+      const recipe = await RecipeModel.findById(id);
+      if (!recipe) {
+        throwCustomError(
+          `Recipe with id ${id} does not exists.`,
+          ErrorTypes.NOT_FOUND,
+        );
+      }
+      return {
+        id: recipe._id,
+        ...recipe._doc,
+      };
+    },
+  },
+};
